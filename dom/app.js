b967053121395ed961582987c3a619d9e8330d61
darshangit <darshans1989@gmail.com>
2021-01-23 20:26:54 -0500

    DOM manipulation

--- dom/app.js
+++ dom/app.js
@@ -1,8 +1,9 @@
-const ul = document.body.firstElementChild.nextElementSibling;
-const firstUl = ul.firstElementChild;
+// const ul = document.body.firstElementChild.nextElementSibling;
+// const firstUl = ul.firstElementChild;
 
-console.log(firstUl);
+// console.log(firstUl);
 
+const ul = document.querySelector('ul');
 const section = document.querySelector('section');
 const button = document.querySelector('button');
 
@@ -18,4 +19,27 @@ button.addEventListener('click', () => {
   // }
 
   section.classList.toggle('invisible'); // beauty
+
+  //ul.innerHTML = ul.innerHTML + '<li>Abc</li>'; // will re-render the whole ul
+  //ul.insertAdjacentHTML('beforeend', '<li> awesome </li>'); // downside is no direct access to the added element
+  const li = document.createElement('li');
+  li.className = 'list-item';
+  li.textContent = 'item 4';
+
+  const l2 = document.createElement('li');
+  l2.className = 'list-item';
+  l2.textContent = 'item 5';
+
+  // ul.append(l2);
+  // ul.prepend(li); // firstelement
+
+  // ul.firstElementChild.before(l1);
+  // ul.lastElementChild.after(l2);
+  // ul.lastElementChild.replaceWith(l2); // not supported on IE and Safari
+
+  const l3 = li.cloneNode(false); // true - will clone all the nested and descendents
+  l3.textContent = 'Item new';
+
+  ul.insertAdjacentElement('beforeend', l3); // better browser support
+  ul.insertAdjacentElement('beforeend', l2); // better browser support
 });
