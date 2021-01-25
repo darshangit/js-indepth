a478fd24fe724c9fc297d0c81b4f244ca252e69c
darshangit <darshans1989@gmail.com>
2021-01-24 19:43:46 -0500

    movie app

--- dom/movies/assets/scripts/app.js
+++ dom/movies/assets/scripts/app.js
@@ -3,11 +3,25 @@ const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
 const cancelButton = addMovieModal.querySelector('.btn--passive');
-const addButton = addMovieModal.querySelector('.btn--success');
+const addButton = cancelButton.nextElementSibling;
+const inputElements = addMovieModal.querySelectorAll('input');
+const entryTextSelection = document.getElementById('entry-text');
+const movies = [];
+
+const updateUI = () => {
+  if (movies.length) {
+    entryTextSelection.style.display = 'none';
+  } else {
+    entryTextSelection.style.display = 'block';
+  }
+};
+
+const renderNewMovieElement = () => {};
 
 const toggleMovieModal = () => {
   addMovieModal.classList.toggle('visible');
   toggleBackDrop();
+  clearMovieInput();
 };
 
 const toggleBackDrop = () => {
@@ -18,8 +32,41 @@ const backDropClickHandler = () => {
   toggleMovieModal();
 };
 
+const clearMovieInput = () => {
+  for (const userInput of inputElements) {
+    userInput.value = '';
+  }
+};
+
+const addMovieHandler = () => {
+  const titleValue = inputElements[0].value;
+  const imageUrlValue = inputElements[1].value;
+  const ratingValue = inputElements[2].value;
+
+  if (
+    titleValue.trim() === '' ||
+    imageUrlValue.trim() === '' ||
+    ratingValue.trim() === '' ||
+    +ratingValue < 1
+  ) {
+    alert('Please enter valid values');
+    return;
+  }
+
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModal();
+  clearMovieInput();
+  updateUI();
+};
+
 startAddMovieButton.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', backDropClickHandler);
 cancelButton.addEventListener('click', toggleMovieModal);
-
-console.log(addMovieModal);
+addButton.addEventListener('click', addMovieHandler);
