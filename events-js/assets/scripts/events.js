a675f0311257b2df0cb7c8c1b2f43985203acc2b
Darshan Satya <darshans1989@gmail.com>
2021-03-16 20:45:41 -0400

    Bubbling and caputuring events

--- events-js/assets/scripts/events.js
+++ events-js/assets/scripts/events.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('button');
+const button = document.querySelector('button');
 
 // NOt recommedded
 // button.onclick = function(){};
@@ -20,26 +20,45 @@ const anotherClickHandler =() => {
 // }, 3000);
 // // button.removeEventListener();
 
-buttons.forEach( btn => {
-    btn.addEventListener('mouseenter', buttonClickHandler);
-})
+// buttons.forEach( btn => {
+//     btn.addEventListener('mouseenter', buttonClickHandler);
+// })
 
 // window.addEventListener('scroll', event => {
 //     console.log(event);
 // })
 
 //infinite scrolling example
-let curElementNumber = 0;
+// let curElementNumber = 0;
  
-function scrollHandler() {
-    const distanceToBottom = document.body.getBoundingClientRect().bottom;
+// function scrollHandler() {
+//     const distanceToBottom = document.body.getBoundingClientRect().bottom;
  
-    if (distanceToBottom < document.documentElement.clientHeight + 150) {
-        const newDataElement = document.createElement('div');
-        curElementNumber++;
-        newDataElement.innerHTML = `<p>Element ${curElementNumber}</p>`;
-        document.body.append(newDataElement);
-    }
-}
+//     if (distanceToBottom < document.documentElement.clientHeight + 150) {
+//         const newDataElement = document.createElement('div');
+//         curElementNumber++;
+//         newDataElement.innerHTML = `<p>Element ${curElementNumber}</p>`;
+//         document.body.append(newDataElement);
+//     }
+// }
 
-window.addEventListener('scroll', scrollHandler);
+// window.addEventListener('scroll', scrollHandler);
+
+
+const form = document.querySelector('form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    console.log(event);
+})
+
+const div = document.querySelector('div');
+div.addEventListener('click', event => {
+    console.log('CLICKED DIV');
+
+}, false) // true will capture the event up to down
+
+button.addEventListener('click', event => {
+    event.stopPropagation();
+    event.stopImmediatePropagation(); //the other listeners on the same button will stop executing
+    console.log('CLICKED BUTTON');
+});
