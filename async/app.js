ca2e6943d96612d1d5e917348a3975db76efe4da
Darshan Satya <darshans1989@gmail.com>
2021-03-21 11:52:35 -0400

    Async/await

--- async/app.js
+++ async/app.js
@@ -24,26 +24,40 @@ const setTimer = (duration) => {
   return promise;
 }
 
-function trackUserHandler() {
+async function trackUserHandler() {
   //chaining
-  getPosition()
-  .then(posData => {
-    console.log(posData);
-    return setTimer(2000); // this promise will finish and that result is passed to the then - taht is why the chaining
-    // can return anything here - need not be promise - internally it will be wrapped into a promise
-  }).catch(err => {
-    console.log('error', err);  //position matters as - al the above will be skipped. below then will be executed. So its best to put it at the end.
-    return 'Ewwww !!! Error';
-  }).then(data => {
-    console.log(data);
-  });
-  console.log('getting position')
+  let posData;
+  let timerData;
+
+  try {
+    posData = await getPosition();
+    timerData = await setTimer(2000);
+  }
+  catch(err){
+    console.log(err);
+  }
+  console.log('posData: ', posData, ', timerData: ', timerData);
+
+  //aync / await internally just transforms like below.
+  // .then(posData => {
+  //   console.log(posData);
+  //   return setTimer(2000); // this promise will finish and that result is passed to the then - taht is why the chaining
+  //   // can return anything here - need not be promise - internally it will be wrapped into a promise
+  // })
+  // .catch(err => {
+  //   console.log('error', err);  //position matters as - al the above will be skipped. below then will be executed. So its best to put it at the end.
+  //   return 'Ewwww !!! Error - But we move forward';
+  // }).then(data => {
+  //   console.log(data);
+  // });
 }
 
 button.addEventListener('click', trackUserHandler);
 
 let result = 0;
 let result2 = 0;
+
+console.log('hola');
 
 // for (let i = 0; i < 10000; i++) {
 //   result += i;
