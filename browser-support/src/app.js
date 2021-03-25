13964e909b9047599d97866dc31dc5af8bd6b9b5
Darshan Satya <darshans1989@gmail.com>
2021-03-25 16:44:20 -0400

    Polyfill integration

--- browser-support/src/app.js
+++ browser-support/src/app.js
@@ -1,3 +1,5 @@
+// import 'core-js/features/promise';
+
 const { result } = require('lodash');
 
 const button = document.querySelector('button');
@@ -5,7 +7,8 @@ const textparagraph = document.querySelector('p');
 
 button.addEventListener('click', () => {
   const text = textparagraph.textContent;
-
+  const promise = new Promise();
+  console.log(promise);
   if (navigator.clipboard) {
     navigator.clipboard
       .writeText(text)
