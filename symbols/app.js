1646595a06a7cde9131b25b4c67b348a2170fe85
Darshan Satya <darshans1989@gmail.com>
2021-03-26 21:04:00 -0400

    Yield + symbols

---
+++ symbols/app.js
@@ -0,0 +1,78 @@
+//Library Land
+const uid = Symbol('uid');
+console.log(uid);
+
+const user = {
+  //id: 'p1',
+  [uid]: 'p1',
+  name: 'Max',
+  age: 30,
+  [Symbol.toStringTag]: 'User',
+};
+
+user[uid] = 'p3';
+
+// app land => using the library
+user.id = 'p2'; // this should not be possible
+
+console.log(user.toString());
+
+const company = {
+  curEmployee: 0,
+  employees: ['Dash', 'Pri', 'Anna'],
+//   next() {
+//     if (this.curEmployee >= this.employees.length) {
+//       return { value: this.curEmployee, done: true };
+//     }
+//     const returnValue = {
+//       value: this.employees[this.curEmployee],
+//       done: false,
+//     };
+//     this.curEmployee++;
+//     return returnValue;
+//   },
+//   getEmployee: function* employeeGenerator() {
+//     // let employee = company.next();
+
+//     // while (!employee.done) {
+//     //   yield employee.value;
+//     //   employee = company.next();
+//     // }
+//     let currentEmployee = 0;
+//     while (currentEmployee < this.employees.length) {
+//       yield this.employees[currentEmployee];
+//       currentEmployee++;
+//     }
+//   },
+
+[Symbol.iterator]: function* employeeGenerator() {
+    // let employee = company.next();
+
+    // while (!employee.done) {
+    //   yield employee.value;
+    //   employee = company.next();
+    // }
+    let currentEmployee = 0;
+    while (currentEmployee < this.employees.length) {
+      yield this.employees[currentEmployee];
+      currentEmployee++;
+    }
+  },
+};
+
+// let employee = company.next();
+
+// while (!employee.done) {
+//   console.log(employee.value);
+//   employee = company.next();
+// }
+//Logic for our own iterator
+//Yield is the magic keyword
+// const it = company.getEmployee();
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+
+for(const employee of company){
+    console.log(employee);
+}
