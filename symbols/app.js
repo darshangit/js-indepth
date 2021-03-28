fe8542bf3be0b2def5f4e8589f372ad3ceac8a26
Darshan Satya <darshans1989@gmail.com>
2021-03-27 20:17:51 -0400

    Reflect API

--- symbols/app.js
+++ symbols/app.js
@@ -20,32 +20,32 @@ console.log(user.toString());
 const company = {
   curEmployee: 0,
   employees: ['Dash', 'Pri', 'Anna'],
-//   next() {
-//     if (this.curEmployee >= this.employees.length) {
-//       return { value: this.curEmployee, done: true };
-//     }
-//     const returnValue = {
-//       value: this.employees[this.curEmployee],
-//       done: false,
-//     };
-//     this.curEmployee++;
-//     return returnValue;
-//   },
-//   getEmployee: function* employeeGenerator() {
-//     // let employee = company.next();
+  //   next() {
+  //     if (this.curEmployee >= this.employees.length) {
+  //       return { value: this.curEmployee, done: true };
+  //     }
+  //     const returnValue = {
+  //       value: this.employees[this.curEmployee],
+  //       done: false,
+  //     };
+  //     this.curEmployee++;
+  //     return returnValue;
+  //   },
+  //   getEmployee: function* employeeGenerator() {
+  //     // let employee = company.next();
 
-//     // while (!employee.done) {
-//     //   yield employee.value;
-//     //   employee = company.next();
-//     // }
-//     let currentEmployee = 0;
-//     while (currentEmployee < this.employees.length) {
-//       yield this.employees[currentEmployee];
-//       currentEmployee++;
-//     }
-//   },
+  //     // while (!employee.done) {
+  //     //   yield employee.value;
+  //     //   employee = company.next();
+  //     // }
+  //     let currentEmployee = 0;
+  //     while (currentEmployee < this.employees.length) {
+  //       yield this.employees[currentEmployee];
+  //       currentEmployee++;
+  //     }
+  //   },
 
-[Symbol.iterator]: function* employeeGenerator() {
+  [Symbol.iterator]: function* employeeGenerator() {
     // let employee = company.next();
 
     // while (!employee.done) {
@@ -73,6 +73,30 @@ const company = {
 // console.log(it.next());
 // console.log(it.next());
 
-for(const employee of company){
-    console.log(employee);
+for (const employee of company) {
+  console.log(employee);
 }
+
+console.log(...company);
+
+const persons = ['Max', 'Manu'];
+console.log(persons);
+
+// ---Reflect API
+
+const course = {
+  title: 'JS - Advanced',
+};
+
+Reflect.setPrototypeOf(course, {
+  toString() {
+    return this.title;
+  },
+});
+
+// Reflect.defineProperty(course, 'price', {});
+// Reflect.deleteProperty();
+// Object.deleteProperty(); // not present in object
+//Reflect APi is new
+// You can use the Reflect API to do meta work on a object - add prototype etc.
+console.log(course);
